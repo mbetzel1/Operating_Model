bf9bc1672a072ed7bc669a7a1b1ff56e7c73de88
mbetz <mbetzel1@gmail.com>
2024-03-04 09:43:04 -0500

    include availability and batch success rate in expected yield

--- optimization.py
+++ optimization.py
@@ -7,22 +7,59 @@ import pandas as pd
 import matplotlib.patches
 from entities import Machine, Buffer
 
+STARTING_NUMBER_OF_EACH_MACHINE = [4,7,11,20,5,4,26,5,3,20]
+
+# Define constants
+SPEC_PATH = "./Machine_Specs.csv"
+specs = pd.read_csv(SPEC_PATH, index_col="Item",)
+machine_names = specs.index
 
 
-def optimize(repititions=5, runs=5, numbers_of_each_machine=[2,6,11,20,5,4,26,5,3,20], target=1000):
+# Number of hours annually in a one or two shift shop
+TWO_SHIFT_ANNUAL_HOURS = specs.loc['Jet-Mill', 'Two-Shift-Annual-Hours']
+ONE_SHIFT_ANNUAL_HOURS = specs.loc['Jet-Mill', 'One-Shift-Annual-Hours']
+SIMULATION_HOURS = TWO_SHIFT_ANNUAL_HOURS
+BATCH_FAILURE_RATE = 0.05
+HOURS_PER_DAY = 24
+WORK_HOURS_PER_DAY = 16
+DAYS_PER_YEAR = 365
+TWO_SHIFT_HOURS_WORKED_PER_DAY = 16
+ONE_SHIFT_HOURS_WORKED_PER_DAY = 8
+DELIVERY_SIZE = 10000
+DELIVERY_SIZE_SIGMA = 0
+DELIVERY_TIME = 1
+DELIVERY_TIME_SIGMA = 0
+
+def optimize(repetitions=5, runs=5, numbers_of_each_machine=[2,6,11,20,5,4,26,5,3,20], target=1000):
     SPEC_PATH = "./Machine_Specs.csv"
     specs = pd.read_csv(SPEC_PATH, index_col="Item",)
     machine_names = specs.index
     reversed_names = machine_names[::-1]
     finished_by_type = {}
-    yield_ratios = {}
+    # this converts each machines input units in terms of the end result of the entire process
+    expected_output_percents = {}
     yield_ratio = 1
+    prev_name = None
     # use the expected yield of each step to know how many pounds you need to get the target
     for name in reversed_names:
-        yield_ratio *= float(specs.loc[name, "Yield"][:2])/100
-        yield_ratios[name] = yield_ratio
+        if prev_name:
+            mtbf = specs.loc[prev_name, "MBTF-Days"] * WORK_HOURS_PER_DAY
+            mttr = specs.loc[prev_name, "MMT-Days"] * WORK_HOURS_PER_DAY
+        else:
+            mtbf = 1
+            mttr = 0
+        batch_success_rate = 1 - BATCH_FAILURE_RATE
+        avail = mtbf/(mtbf+mttr)
+        yield_percent = float(specs.loc[name, "Yield"][:2])/100
+        yield_ratio *= batch_success_rate * yield_percent
+        # We put availability here, because only the bottleneck will be limited by availability
+        # Therefore we do not want to keep a running tally like we do with yield
+        expected_output_percents[name] = yield_ratio * avail
+        prev_name = name
+        print(f'{name} yield ratio is {expected_output_percents[name]} with avail of {avail}, batch succes of {batch_success_rate} and yield of {yield_percent}')
+
     # go through reps changing the number of each machine
-    for i in range(repititions):
+    for i in range(repetitions):
         # set the dict back to zero
         for name in machine_names:
             finished_by_type[name] = 0.0
@@ -32,7 +69,7 @@ def optimize(repititions=5, runs=5, numbers_of_each_machine=[2,6,11,20,5,4,26,5,
         for j in range(runs):
             machine_dict = list_of_dicts[j]
             for name in machine_names:
-                finished_by_type[name] += yield_ratios[name] * sum([machine.number_finished for machine in machine_dict[name]]) /(2000 * runs)
+                finished_by_type[name] += expected_output_percents[name] * sum([machine.number_finished for machine in machine_dict[name]]) /(2000 * runs)
                 for machine in machine_dict[name]:
                     print(f'{machine.name} utilization rate: {(machine.utilization*100):.2f} %')
         # for the first machine which didn't finish the target on average, add one more
@@ -56,23 +93,6 @@ def run_process(numbers_of_each_machine=[2,6,11,20,5,4,26,5,3,20]):
     SPEC_PATH = "./Machine_Specs.csv"
     specs = pd.read_csv(SPEC_PATH, index_col="Item",)
     machine_names = specs.index
-
-    # Define constants
-    HOURS_PER_DAY = 24
-    WORK_HOURS_PER_DAY = 16
-    DAYS_PER_YEAR = 365
-    TWO_SHIFT_HOURS_WORKED_PER_DAY = 16
-    ONE_SHIFT_HOURS_WORKED_PER_DAY = 8
-    DELIVERY_SIZE = 10000
-    DELIVERY_SIZE_SIGMA = 0
-    DELIVERY_TIME = 1
-    DELIVERY_TIME_SIGMA = 0
-
-    # Number of hours annually in a one or two shift shop
-    TWO_SHIFT_ANNUAL_HOURS = specs.loc['Jet-Mill', 'Two-Shift-Annual-Hours']
-    ONE_SHIFT_ANNUAL_HOURS = specs.loc['Jet-Mill', 'One-Shift-Annual-Hours']
-
-    SIMULATION_HOURS = TWO_SHIFT_ANNUAL_HOURS
 
 
 
@@ -109,7 +129,7 @@ def run_process(numbers_of_each_machine=[2,6,11,20,5,4,26,5,3,20]):
             cycle_time_sigma = specs.loc[name, 'Cycle-Time']/10,
             yield_rate = float(specs.loc[name, "Yield"][:2])/100,
             yield_sigma = float(specs.loc[name, "Yield"][:2])/100/20,
-            batch_failure_rate = 0.05,
+            batch_failure_rate = BATCH_FAILURE_RATE,
             mtbf = specs.loc[name, "MBTF-Days"] * WORK_HOURS_PER_DAY,
             mttr = specs.loc[name, "MMT-Days"] * WORK_HOURS_PER_DAY,
             repair_std_dev= 0.5,
@@ -128,4 +148,4 @@ def run_process(numbers_of_each_machine=[2,6,11,20,5,4,26,5,3,20]):
 #         print(f'{machine.name} finished {machine.number_finished} lbs')
 # for buffer in buffer_list:
 #     print(buffer.name + " has a level of " + str(buffer.level))
-print(optimize(5,5, [4,8,14,25,7,6,29,10,6,25]))
+print(optimize(3,3, STARTING_NUMBER_OF_EACH_MACHINE))
